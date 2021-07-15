d5f0e8f898df0ea3c76ffae675d9c466149bcef9
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2021-07-15 20:42:45 +0900

    Fix remaining Rubocop errors

--- spec/druby_job_spec.rb
+++ spec/druby_job_spec.rb
@@ -1,15 +1,15 @@
 # frozen_string_literal: true
 
-RSpec.describe HardWorker do
-  class DummyWorker
-    attr_accessor :queue
+class DummyWorker
+  attr_accessor :queue
 
-    def initialize
-      server_uri = HardWorker::URI
-      self.queue = DRbObject.new_with_uri(server_uri)
-    end
+  def initialize
+    server_uri = HardWorker::URI
+    self.queue = DRbObject.new_with_uri(server_uri)
   end
+end
 
+RSpec.describe HardWorker do
   describe 'job processing' do
     it 'allows you to run code in the background' do
       worker = Thread.new { HardWorker.new(connect: true) }
