329e85cdf5455779bf33d3fef9a64209b3963cf1
Sampo Kuokkanen <sampo.kuokkanen@gmail.com>
2021-07-19 22:25:52 +0900

    Remove rails/test_help

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,4 +7,3 @@ require 'rails'
 require_relative '../dummy/config/environment'
 ActiveRecord::Migrator.migrations_paths = [File.expand_path('../dummy/db/migrate', __dir__)]
 ActiveRecord::Migrator.migrations_paths << File.expand_path('../db/migrate', __dir__)
-require 'rails/test_help'
